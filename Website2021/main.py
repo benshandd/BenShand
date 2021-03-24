fd6033168fec45b56e2993b4ab806ad9538ad37a
sillycoffin <benjaminshand101@gmail.com>
2021-03-24 17:46:07 +1300

    Commit

--- Website2021/main.py
+++ Website2021/main.py
@@ -9,12 +9,26 @@ app = Flask(__name__)
 def home():
         conn = sqlite3.connect('CreatePizza.db')
         c = conn.cursor()
-        #query selects all from DiscountBox table
+        #query selects all
         c.execute("SELECT * FROM Pizza ;")
         fetch = c.fetchall()
         conn.close()
         return render_template("home.html", fetch=fetch)
 
+@app.route('/hello/<int:id>')
+def single_discountbox(id):
+        conn = sqlite3.connect('AllFruitsDatabase.db')
+        c = conn.cursor()
+        #query selects all from Fruit table and grabs corelating information for the displayed discount box id
+        c.execute("SELECT * FROM DiscountBox WHERE id=?;",(id,))
+        discountboxes = c.fetchone()
+        conn.close()
+        return render_template("single_discountbox.html", discountboxes=discountboxes)
+
+#reroutes 404 errrors
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
